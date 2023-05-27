9004d4354f9e2bd5a6d8af777acfc9ba0e105e65
Cristianbenz <contactocbenitez@gmail.com>
2023-05-27 10:27:39 -0300

    display user name and avatar and add dialog form to change picture

--- src/app/components/userHeader/userHeader.component.ts
+++ src/app/components/userHeader/userHeader.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { User } from 'src/app/models/user';
 import { ApiAuthService } from 'src/app/services/apiAuthService';
+import { AvatarDialogComponent } from '../avatar-dialog/avatar-dialog.component';
 
 @Component({
   selector: 'user-header-component',
@@ -7,9 +10,21 @@ import { ApiAuthService } from 'src/app/services/apiAuthService';
   styleUrls: ['./userHeader.component.scss']
 })
 export class UserHeaderComponent {
-  private authService = inject(ApiAuthService);
+  public user: null | User = null;
+  private _authService = inject(ApiAuthService);
+  private _dialogRef = inject(MatDialog);
   
+  constructor() {
+    this._authService.user.subscribe(info => this.user = info)
+  }
+
   signOut() {
-    this.authService.signOut()
+    this._authService.signOut()
+  }
+
+  openAvatarForm() {
+    this._dialogRef.open(AvatarDialogComponent, {
+      data: this.user
+    })
   }
 }
---
+++ src/app/services/apiPictureService.ts
@@ -0,0 +1,17 @@
+import { Injectable, inject } from '@angular/core';
+import { Response } from '../models/response';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiPictureService {
+  private _api = environment.apiUrl + 'api';
+  private readonly _http = inject(HttpClient);
+
+  uploadAvatar(userId: number, image: FormData): Observable<Response> {
+    return this._http.post<Response>(`${this._api}/Image/${userId}`, image);
+  }
+}
